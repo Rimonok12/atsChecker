78d0c04684cc08c3678f76673171589e522628a2
RIMON DEBNATH <68506201+Rimonok12@users.noreply.github.com>
2025-08-04 17:01:55 +0600

    2nd

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,49 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import Navbar from 'components/Navbar';
+import type { Route } from './+types/home';
+import ResumeCard from 'components/ResumeCard';
+import { resumes } from '~/constants';
+import { useEffect } from 'react';
+import { usePuterStore } from '~/lib/puter';
+import { useNavigate } from 'react-router';
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'getIn10minCv' },
+    { name: 'description', content: 'Beat the Bots. Land the Interview.' },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Only redirect if user is NOT authenticated
+    if (!auth.isAuthenticated) {
+      navigate('/auth?next=/');
+    }
+  }, [auth?.isAuthenticated]);
+
+  return (
+    <main
+      className="bg-[url('/images/bg-main.svg')] bg-cover 
+      "
+      // min-h-screen flex items-center justify-center
+    >
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Is Your Resume ATS-Friendly? Get Score & Rating </h1>
+          <h2> Review Your Submission & Get AI-Powered Feedback</h2>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
